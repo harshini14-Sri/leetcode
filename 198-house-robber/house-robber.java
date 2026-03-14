83a35c26c0c7d1f8556f464956fdf065d7156c44
harshini <peddisriharshini@gmail.com>
2026-03-14 15:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (32.18%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<nums.length;i++){
+            int temp=Math.max(sum1,sum2+nums[i]);
+            sum2=sum1;
+            sum1=temp;
+        }
+        return sum1;
+    }
+}
